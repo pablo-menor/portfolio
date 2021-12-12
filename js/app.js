02cd8efee93965d7b8e136879ae3d63fc1f016db
Pablo <pablomenor2001@gmail.com>
2021-12-12 18:45:22 +0100

    home done

--- js/app.js
+++ js/app.js
@@ -22,3 +22,29 @@
         link.addEventListener('click', () => {
             menu.classList.remove('open-menu');
         }))
+
+    //-----------------TYPING EFFECT------------------------------
+
+    const texts = ['web developer', 'full-stack developer']
+    let count = 0;
+    let index = 0;
+    let currentText = '';
+    let letter = '';
+   
+  (function type() {
+      if (count === texts.length) {
+          count = 0;
+      }
+
+      currentText = texts[count];
+      letter = currentText.slice(0, ++index);
+      document.querySelector('.typing').textContent = letter;
+
+      if (letter.length === currentText.length) {
+          count++;
+          index = 0;
+      }
+
+      setTimeout(type, 170);
+
+  }())
