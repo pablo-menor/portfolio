356ff7afcdf8c7eb24d47c9a29768f9570baef11
Pablo <pablomenor2001@gmail.com>
2021-12-14 20:59:43 +0100

    phone version done

--- js/app.js
+++ js/app.js
@@ -3,17 +3,26 @@
     const menuIcon = document.querySelector('.material-icons.menu');
     const menu = document.querySelector('.menu-phone');
     const linksMenu = document.querySelectorAll('.linkMenu');
+    const sections = document.querySelectorAll('section');
+    const footer = document.querySelector('footer');
     let opened = false;
 
 
     menuIcon.addEventListener('click', () => {
+      
         if (opened) {
             menu.classList.remove('open-menu');
             opened = false;
+            sections.forEach(s => s.style.display= '');
+            footer.style.display= '';
         }
         else if (!opened) {
             menu.classList.add('open-menu');
             opened = true;
+           
+            sections.forEach(s => s.style.display= 'none');
+            footer.style.display= 'none';
+
         }
 
     })
@@ -21,6 +30,8 @@
     linksMenu.forEach(link =>
         link.addEventListener('click', () => {
             menu.classList.remove('open-menu');
+            sections.forEach(s => s.style.display= '');
+            footer.style.display= '';
         }))
 
     //-----------------TYPING EFFECT------------------------------
@@ -48,3 +59,37 @@
       setTimeout(type, 170);
 
   }())
+
+  //-----------------INTERSECTION OBSERVER------------------------------
+//   let options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 1.0
+//   }
+//   let jeje = ()=>{
+//       console.log('jeje');
+//   }
+//   let target = document.querySelector('.projects');
+//   let observer = new IntersectionObserver(jeje,options);
+
+  
+// observer.observe(target);
+
+//----------------Copy email to clipboard------------------------------
+
+const email = document.querySelector('.email');
+
+email.addEventListener('mouseenter', () => {
+    email.style.backgroundColor = "#00B567";
+})
+email.addEventListener('mouseleave', () => {
+    if (email.innerHTML != 'Copied!')
+    email.style.backgroundColor = "#424242";
+
+})
+
+email.addEventListener('click', () => {
+    email.innerHTML = `Copied!`;
+    email.style.backgroundColor = "#00B567";
+    navigator.clipboard.writeText('pablomenordeonate@gmail.com');
+})
